9cbeb93d0b9a7d99b03da8adc195c0d83ab61528
plagentile1121@gmail.com <plagentile1121@gmail.com>
2020-04-10 23:17:15 -0500

    Project is now Maven

---
+++ WeWorkFiveTimesAsHard/WorkFlowManager/src/test/java/com/MainDriver/WorkFlowManager/WorkFlowManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MainDriver.WorkFlowManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkFlowManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
