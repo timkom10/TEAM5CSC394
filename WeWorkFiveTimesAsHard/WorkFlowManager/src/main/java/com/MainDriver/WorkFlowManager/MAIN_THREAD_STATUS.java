e3a0f801c41806d7b42984a5b9c51054b02afd7d
plagentile1121@gmail.com <plagentile1121@gmail.com>
2020-04-11 04:50:48 -0500

    Lomboks and enums

---
+++ WeWorkFiveTimesAsHard/WorkFlowManager/src/main/java/com/MainDriver/WorkFlowManager/MAIN_THREAD_STATUS.java
@@ -0,0 +1,11 @@
+package com.MainDriver.WorkFlowManager;
+
+/*
+    Demonstrates the practice of favoring enumerations over exceptions
+ */
+public enum MAIN_THREAD_STATUS
+{
+    EXITSUCCESS,        //0
+    FAILEDCHECKSUM      //1
+}
+
