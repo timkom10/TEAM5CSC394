75c76fd2deae6214ceefb8df07045f317824bd2f
plagentile1121@gmail.com <plagentile1121@gmail.com>
2020-05-11 19:02:53 -0500

    Prep for login

---
+++ WeWorkFiveTimesAsHard/WorkFlowManager/src/main/java/com/MainDriver/WorkFlowManager/Controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.MainDriver.WorkFlowManager.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+
+    @RequestMapping("/")
+    public String getLogin(Model model)
+    {
+
+        return "WelcomeView/Login";
+    }
+}
